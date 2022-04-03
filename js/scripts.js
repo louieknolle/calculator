7b0d641f723efad38bbb4d172ade0155bf6e2dfb
Louis Knolle <knollelw@gmail.com>
2022-04-03 18:12:56 -0400

    add forms for other operators
    Co-authored by: Sue Roberts <suerobertstx@yahoo.com>
    Co-authored by: Joel Lake <joelcooperlake@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -22,6 +22,6 @@ $(document).ready(function() {
     const number1 = parseInt($("#add1").val());
     const number2 = parseInt($("#add2").val());
     const result = add(number1, number2);
-    $("#output").text(result);
+    $("#output-add").text(result);
   });
 });
